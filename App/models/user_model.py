7014e87641fc99c729ec7124820e83fb7fb33084
sri pavani kona <konasripavani99@gmail.com>
2025-06-30 19:16:30 +0530

    Add files via upload

---
+++ App/models/user_model.py
@@ -0,0 +1,16 @@
+# app/models/user_model.py
+
+import json
+import os
+
+USER_FILE = "users.json"
+
+def load_users():
+    if os.path.exists(USER_FILE):
+        with open(USER_FILE, "r") as f:
+            return json.load(f)
+    return {}
+
+def save_users(users):
+    with open(USER_FILE, "w") as f:
+        json.dump(users, f, indent=4)
